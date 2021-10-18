1a8d606b403e65a0bd1674aa8a3691388ccb68d0
Newaz <newaz@mobilityonegroup.com>
2021-10-19 03:04:56 +0600

    Start Developing Dashboard

---
+++ Dos4PeopleApp/wwwroot/Application_Scripts/Dashboard.js
@@ -0,0 +1,12 @@
+﻿$('#copyBtn').click(function () {
+    debugger;
+    /* Get the text field */
+    var copyText = $('#refferelId').text();
+
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText);
+
+    /* Alert the copied text */
+    toastr.info("Copied the text: " + copyText);
+})
