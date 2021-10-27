81a22a0c65815b1497ff854f7c4220761d4ea181
Newaz <newaz@mobilityonegroup.com>
2021-10-28 00:24:52 +0600

    Plan Development started

---
+++ Dos4PeopleApp/wwwroot/Application_Scripts/Plan.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+});
+//var planListRender = function () {
+//    $.ajax({
+//        url: "/User/InsertUser",
+//        data: JSON.stringify(objVmUser),
+//        type: "POST",
+//        contentType: "application/json;charset=utf-8",
+//        success: function (response) {
+//            debugger;
+//            if (response.status == true) {
+//                //toastr.success(response.message);
+//                $('#UserName').val('');
+//                $('#Email').val('');
+//                $('#Password').val('');
+//                $('#ConfirmPassword').val('');
+//                $('#Referrer').val('');
+//                $('#successAlert').show();
+//            } else {
+//                toastr.error(response.message);
+//            }
+//        },
+//        error: function (response) {
+//            toastr.error("error!");
+//        }
+//}
